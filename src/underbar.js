3ddebf4b088013528394ffaf6e63e73cc21e0584
liebeskind <daniel.liebeskind@gmail.com>
2013-08-28 16:42:24 -0700

    implemented the _.unique function by using the _.indexOf function

--- src/underbar.js
+++ src/underbar.js
@@ -80,8 +80,11 @@ var _ = { };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
-
-    
+    var result = [];
+    for (var i=0; i<array.length;i++){
+      if (_.indexOf(result, array[i]) == -1) result.push(array[i]);  
+    }
+   return result;
   };
 
 
