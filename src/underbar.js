0f4831c70ed083b11ef57c7de2141ee279f9eeba
liebeskind <daniel.liebeskind@gmail.com>
2013-08-28 16:09:07 -0700

    -Updated Underbar.js for _.indexOf function (which the instructions indicated should have been provided?) -Created _.filter function

--- src/underbar.js
+++ src/underbar.js
@@ -50,10 +50,20 @@ var _ = { };
     // TIP: Here's an example of a function that needs to iterate, which we've
     // implemented for you. Instead of using a standard `for` loop, though,
     // it uses the iteration helper `each`, which you will need to write.
+
+    if (array == null) {
+      return -1; }
+    
+    for (var i =0; i < array.length; i++) if (array[i] === target) return i; return -1;
   };
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, iterator) {
+    var result = [];
+    for (var num in collection){
+      if (iterator(num)==false) result.push(collection[num])
+    }
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
