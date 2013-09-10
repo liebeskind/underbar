e92efbe3710d9f0f28cfd9150e091dd9a20b975c
Daniel Liebeskind <daniel.liebeskind@gmail.com>
2013-09-10 15:11:04 -0700

    updated the _.indexOf function, but having difficulty getting duplicate values to pass

--- src/underbar.js
+++ src/underbar.js
@@ -50,10 +50,17 @@ var _ = { };
     // TIP: Here's an example of a function that needs to iterate, which we've
     // implemented for you. Instead of using a standard `for` loop, though,
     // it uses the iteration helper `each`, which you will need to write.
-
-    if (array == null) {
-      return -1; }
-    for (var i =0; i < array.length; i++) if (array[i] === target) return i; return -1;
+    var result = -1;
+    if (array === null){
+    	return -1;
+    } else {
+    _.each(array, function(value, index, list){
+			if (value === target){
+				return result = index*1;
+			}
+	})
+}
+	return result;
   };
 
   // Return all elements of an array that pass a truth test.
